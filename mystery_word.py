a0f9a240f83c6af7be17e1f744d9eb001272f027
John Parrish <Johnwillparrish3@gmail.com>
2022-06-22 15:32:12 -0400

    cleaned up code and random word working

--- mystery_word.py
+++ mystery_word.py
@@ -1,11 +1,13 @@
 import random
 
+
 def random_word():
     file = open('test-word.txt')
     read_word = file.read()
     random_list_word = read_word.split()
     computer_word = random.choice(random_list_word)
     return computer_word
+
 
 def word_list(file):
     word_as_list = []
@@ -14,8 +16,12 @@ def word_list(file):
     # print(f'word as list: {word_as_list}')
     return word_as_list
 
+
 def show_blanks(file):
-    print('_ ' * len(file))
+    display_blanks = list('_ ' * len(file))
+    print(display_blanks)
+    return '_ ' * len(file)
+
 
 def get_guess():
     user_guess = input('Guess a letter: ')
@@ -24,27 +30,34 @@ def get_guess():
         print(f'your guess is {user_guess}')
         return user_guess
     else:
-        print(f'Your guess is not a single letter.')
+        print('Your guess is not a single letter.')
         return get_guess()
+
 
 def compare_guess_to_word(word):
     remaining_guesses = 8
+    correct_guesses = []
+    incorrect_guesses = []
     print(f'you have {remaining_guesses} tries to guess the word.')
     while remaining_guesses > 0:
         user_guess = get_guess()
         if user_guess in word:
             print(f'{user_guess} is in the Mystery Word!')
+            correct_guesses.append(user_guess)
+
         else:
-            print (f'{user_guess} is not in the Mystery Word!')
-            remaining_guesses -=1
+            print(f'{user_guess} is not in the Mystery Word!')
+            remaining_guesses -= 1
+            incorrect_guesses.append(user_guess)
             print(f'you have {remaining_guesses} guesses left!')
+
 
 def play_game():
     # random_word()
     mystery_word = random_word()
     make_list = word_list(mystery_word)
     print(f'word: {make_list}')
-    show_blanks(make_list)
+    display = show_blanks(make_list)
     compare_guess_to_word(make_list)
 
 
