ce3646f5956f4bec0b25de1262f4e42c6e40cef6
John Parrish <Johnwillparrish3@gmail.com>
2022-06-22 14:54:47 -0400

    cleaned up code and random word working

--- mystery_word.py
+++ mystery_word.py
@@ -1,20 +1,21 @@
 import random
 
-# def random_word():
-#     file = open('words.txt')
-#     random_list_word = random.choice(file.readlines())
-#     print(random.choice(file.readlines()))
+def random_word():
+    file = open('test-word.txt')
+    read_word = file.read()
+    random_list_word = read_word.split()
+    computer_word = random.choice(random_list_word)
+    return computer_word
 
-
-def word_list(mystery_word):
+def word_list(file):
     word_as_list = []
-    for letter in mystery_word:
+    for letter in file:
         word_as_list.append(letter)
-    print(f'word as list: {word_as_list}')
+    # print(f'word as list: {word_as_list}')
     return word_as_list
 
-def show_blanks(mystery_word):
-    print('_ ' * len(mystery_word))
+def show_blanks(file):
+    print('_ ' * len(file))
 
 def get_guess():
     user_guess = input('Guess a letter: ')
@@ -26,29 +27,25 @@ def get_guess():
         print(f'Your guess is not a single letter.')
         return get_guess()
 
-def compare_guess_to_word(guess, word):
-    if guess in word:
-        print(f'{guess} is in the Mystery Word!')
-        
-    else:
-        print (f'{guess} is not in the Mystery Word!')
-    return guess
+def compare_guess_to_word(word):
+    remaining_guesses = 8
+    print(f'you have {remaining_guesses} tries to guess the word.')
+    while remaining_guesses > 0:
+        user_guess = get_guess()
+        if user_guess in word:
+            print(f'{user_guess} is in the Mystery Word!')
+        else:
+            print (f'{user_guess} is not in the Mystery Word!')
+            remaining_guesses -=1
+            print(f'you have {remaining_guesses} guesses left!')
 
 def play_game():
     # random_word()
-    mystery_word = "dream"
-    print(f'word: {mystery_word}')
-    remaining_guesses = 8
-    print(f'you have {remaining_guesses} tries to guess the word.')
-    show_blanks(mystery_word)
-    while remaining_guesses > 0:
-        user_guess = get_guess()
-        compare_guess_to_word(user_guess, mystery_word)
-        remaining_guesses = remaining_guesses - 1
-        print(f'you have {remaining_guesses} guesses left!')
-    # else:
-    #     remaining_guesses = remaining_guesses - 1
-    #     print(f'You have {remaining_guesses} remaining guess!')
+    mystery_word = random_word()
+    make_list = word_list(mystery_word)
+    print(f'word: {make_list}')
+    show_blanks(make_list)
+    compare_guess_to_word(make_list)
 
 
 if __name__ == "__main__":
