e10fb9ffa399c00644d05cf2f96fd3264fcbbd7d
John Parrish <Johnwillparrish3@gmail.com>
2022-06-22 23:15:20 -0400

    working code but want to add yes or no to play game

--- mystery_word.py
+++ mystery_word.py
@@ -2,7 +2,7 @@ import random
 
 
 def random_word():
-    file = open('test-word.txt')
+    file = open('words.txt')
     read_word = file.read()
     random_list_word = read_word.split()
     computer_word = random.choice(random_list_word)
@@ -35,29 +35,48 @@ def get_guess():
 
 
 def compare_guess_to_word(word):
+    # mystery_word = random_word()
+    # make_list = word_list(mystery_word)
+    display = [(user_guess.replace(user_guess, '_')) for user_guess in word]
+    print(f"this is display: {' '.join(display)}")
     remaining_guesses = 8
     correct_guesses = []
     incorrect_guesses = []
     print(f'you have {remaining_guesses} tries to guess the word.')
-    while remaining_guesses > 0:
+    while len(incorrect_guesses) < 8:
         user_guess = get_guess()
         if user_guess in word:
-            print(f'{user_guess} is in the Mystery Word!')
-            correct_guesses.append(user_guess)
-
+            if user_guess not in correct_guesses or user_guess not in incorrect_guesses:
+                print(f'{user_guess} is in the Mystery Word!')
+                correct_guesses.append(user_guess)
+                display = [(user_guess.replace(user_guess, "_")) if user_guess not in correct_guesses else user_guess for user_guess in word]
+                print(f"this is display: {' '.join(display)}")
+                if set(sorted(display)) == set(sorted(correct_guesses)):
+                    print('YOU WIN!')
+                    break
+            else:
+                print('You already guessed that try again.')
         else:
             print(f'{user_guess} is not in the Mystery Word!')
             remaining_guesses -= 1
             incorrect_guesses.append(user_guess)
             print(f'you have {remaining_guesses} guesses left!')
+        all_guesses = set(correct_guesses + incorrect_guesses)
+        print(f"Letter's already guessed: {' '.join(all_guesses)}")
+    if remaining_guesses == 0:
+        print(f'YOU LOST! The correct word was {word}')
+        y_o_n = input('Would you like to play agian? Y or N')
+        if y_o_n == "y" or y_o_n == "Y":
+            play_game()
+        else:
+            break
+        
 
 
 def play_game():
-    # random_word()
     mystery_word = random_word()
     make_list = word_list(mystery_word)
-    print(f'word: {make_list}')
-    display = show_blanks(make_list)
+    print(f'word: {"".join(make_list)}')
     compare_guess_to_word(make_list)
 
 
